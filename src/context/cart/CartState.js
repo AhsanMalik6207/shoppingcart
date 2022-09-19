ed1a2bd2410b4d139682e1cd9ec81670555c21ff
Ahsan_Malik <ahsanmalik6229@gmail.com>
2022-09-19 06:54:43 -0700

    done

---
+++ src/context/cart/CartState.js
@@ -0,0 +1,42 @@
+import { useReducer } from "react";
+import CartContext from "./CartContext";
+import CartReducer from "./CartReducer";
+import { SHOW_HIDE_CART, ADD_TO_CART, REMOVE_ITEM } from "../Types";
+
+const CartState = ({ children }) => {
+  const initalState = {
+    showCart: false,
+    cartItems: [],
+  };
+
+  const [state, dispatch] = useReducer(CartReducer, initalState);
+
+  const addToCart = (item) => {
+    dispatch({ type: ADD_TO_CART, payload: item });
+  };
+
+  const showHideCart = () => {
+    dispatch({ type: SHOW_HIDE_CART });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: id });
+  };
+
+  return (
+    // The Provider component accepts a value prop to be passed to consuming components that are descendants of this Provider.
+    <CartContext.Provider
+      value={{
+        showCart: state.showCart,
+        cartItems: state.cartItems,
+        addToCart, 
+        showHideCart,
+        removeItem,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartState;
